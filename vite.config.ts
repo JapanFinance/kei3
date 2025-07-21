fcb1dc763442d9bd0fb1c3ec565e4288a766af36
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2025-07-21 16:12:16 +0900

    Cloudflare deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
+import { cloudflare } from "@cloudflare/vite-plugin";
 import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+      cloudflare(),
       react(),
       visualizer({
         filename: 'dist/stats.html',
